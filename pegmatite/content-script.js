600ab9923c04acddbf51ceff2f6228fc650f521c
Daisuke Miyamoto <dai.0304@gmail.com>
2017-12-21 12:08:29 +0900

    refactor

--- pegmatite/content-script.js
+++ pegmatite/content-script.js
@@ -2,8 +2,8 @@ function encode64(data) {
 	for (var r = "", i = 0, n = data.length; i < n; i += 3) {
 		r += append3bytes(
 			data.charCodeAt(i),
-			i + 1 != n ? data.charCodeAt(i + 1) : 0,
-			i + 2 != n ? data.charCodeAt(i + 2) : 0);
+			i + 1 !== n ? data.charCodeAt(i + 1) : 0,
+			i + 2 !== n ? data.charCodeAt(i + 2) : 0);
 	}
 	return r;
 }
@@ -13,12 +13,10 @@ function append3bytes(b1, b2, b3) {
 	var c2 = ((b1 & 0x3) << 4) | (b2 >> 4);
 	var c3 = ((b2 & 0xF) << 2) | (b3 >> 6);
 	var c4 = b3 & 0x3F;
-	var result =
-		encode6bit(c1 & 0x3F) +
+	return encode6bit(c1 & 0x3F) +
 		encode6bit(c2 & 0x3F) +
 		encode6bit(c3 & 0x3F) +
 		encode6bit(c4 & 0x3F);
-	return result;
 }
 
 function encode6bit(b) {
@@ -28,8 +26,8 @@ function encode6bit(b) {
 	b -= 26;
 	if (b < 26) return String.fromCharCode(97 + b);
 	b -= 26;
-	if (b == 0) return "-";
-	if (b == 1) return "_";
+	if (b === 0) return "-";
+	if (b === 1) return "_";
 	return "?";
 }
 
@@ -50,7 +48,7 @@ function escapeHtml(text) {
 [].forEach.call(document.querySelectorAll("pre[lang='uml']"), function(umlElem) {
 	var parent = umlElem.parentNode;
 	var plantuml = umlElem.querySelector("code").textContent.trim();
-	if (plantuml.substr(0, "@start".length) != "@start") return;
+	if (plantuml.substr(0, "@start".length) !== "@start") return;
 
 	var url = "https://www.plantuml.com/plantuml/img/" + compress(plantuml);
 	var imgElem = document.createElement("img");
