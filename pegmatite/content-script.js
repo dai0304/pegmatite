801ee1dbdb5a92cae4687eab4cc6d8be7c1cbd1d
TAKAYOSHI ADACHI <t-adachi@dip-net.co.jp>
2019-12-03 17:33:16 +0900

    fix at eslint
    fix eslint error

--- pegmatite/content-script.js
+++ pegmatite/content-script.js
@@ -47,53 +47,54 @@ function escapeHtml(text) {
 		.replace(/'/g, "&#039;");
 }
 
-function getBackgroundColor(element, pseudoElt = null) {
-    if (element !== null) {
-        return window
-            .getComputedStyle(element, pseudoElt)
-            .getPropertyValue("background-color");
-    }
-    return "";
+function getBackgroundColor(element, pseudoElt) {
+	if (element !== null) {
+		if (pseudoElt === undefined) pseudoElt = null;
+		return window
+			.getComputedStyle(element, pseudoElt)
+			.getPropertyValue("background-color");
+	}
+	return "";
 }
 
 function CodePre(nodeList) {
-    this.exist = false;
-    this.list = nodeList;
-    this.parentColor = "";
-    this.selfColor = "";
-    if (this.list.length > 0) {
-        this.selfColor = getBackgroundColor(this.list.item(0));
-        this.parentColor = getBackgroundColor(this.list.item(0).parentElement);
-        this.exist = true;
-    }
+	this.exist = false;
+	this.list = nodeList;
+	this.parentColor = "";
+	this.selfColor = "";
+	if (this.list.length > 0) {
+		this.selfColor = getBackgroundColor(this.list.item(0));
+		this.parentColor = getBackgroundColor(this.list.item(0).parentElement);
+		this.exist = true;
+	}
 }
 
 var codePre = new CodePre(document.querySelectorAll(".markdown-body pre")); // github style
 
 function changeBackgroundColor(element, color, exist) {
-    if (exist) {
-        element.style.backgroundColor = color;
-    }
+	if (exist) {
+		element.style.backgroundColor = color;
+	}
 }
 
 function replaceElement(umlElem, srcUrl) {
-    var parent = umlElem.parentNode;
-    if (parent !== null) { // for asciidoc (div div pre)
-        var imgElem = document.createElement("img");
-        imgElem.setAttribute("src", escapeHtml(srcUrl));
-        imgElem.setAttribute("title", "");
-        parent.replaceChild(imgElem, umlElem);
-        changeBackgroundColor(parent, codePre.parentColor, codePre.exist);
+	var parent = umlElem.parentNode;
+	if (parent !== null) { // for asciidoc (div div pre)
+		var imgElem = document.createElement("img");
+		imgElem.setAttribute("src", escapeHtml(srcUrl));
+		imgElem.setAttribute("title", "");
+		parent.replaceChild(imgElem, umlElem);
+		changeBackgroundColor(parent, codePre.parentColor, codePre.exist);
 
-        imgElem.ondblclick = function() {
-            parent.replaceChild(umlElem, imgElem);
-            changeBackgroundColor(parent, codePre.selfColor, codePre.exist);
-        };
-        umlElem.ondblclick = function() {
-            parent.replaceChild(imgElem, umlElem);
-            changeBackgroundColor(parent, codePre.parentColor, codePre.exist);
-        };
-    }
+		imgElem.ondblclick = function() {
+			parent.replaceChild(umlElem, imgElem);
+			changeBackgroundColor(parent, codePre.selfColor, codePre.exist);
+		};
+		umlElem.ondblclick = function() {
+			parent.replaceChild(imgElem, umlElem);
+			changeBackgroundColor(parent, codePre.parentColor, codePre.exist);
+		};
+	}
 }
 
 var siteProfiles = {
@@ -161,20 +162,20 @@ var siteProfiles = {
 			return elem.innerText.trim();
 		}
 	},
-    "github.com": { // markdown + asciidoc
-        "selector": "pre[lang='uml'], pre[lang='puml'], pre[lang='plantuml'], div div pre", // markdown, asciidoc
+	"github.com": { // markdown + asciidoc
+		"selector": "pre[lang='uml'], pre[lang='puml'], pre[lang='plantuml'], div div pre", // markdown, asciidoc
 		"extract": function (elem) {
-            var child = elem.querySelector("code");
+			var child = elem.querySelector("code");
 			if (child != null) return child.textContent.trim(); // markdown
-            return elem.textContent.trim(); // asciidoc
+			return elem.textContent.trim(); // asciidoc
 		},
 		"replace": function (elem) {
-            var child = elem.querySelector("code");
+			var child = elem.querySelector("code");
 			if (child != null) return child; // markdown
 			return elem; // asciidoc
 		},
 		"compress": function (elem) {
-            plantuml = ""
+			var plantuml = "";
 			var child = elem.querySelector("code");
 			if (child != null) { // markdown
 				plantuml = elem.querySelector("code").textContent.trim();
