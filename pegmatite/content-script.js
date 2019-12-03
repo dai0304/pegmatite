534131e87b58ecc775bf8b5d7cb0e7b17a6e29ce
TAKAYOSHI ADACHI <t-adachi@dip-net.co.jp>
2019-12-03 17:39:35 +0900

    fix at eslint
    fix eslint error

--- pegmatite/content-script.js
+++ pegmatite/content-script.js
@@ -35,7 +35,7 @@ function encode6bit(b) {
 
 function compress(s) {
 	s = unescape(encodeURIComponent(s));
-	return encode64(RawDeflate.deflate(s));
+	return encode64(window.RawDeflate.deflate(s));
 }
 
 function escapeHtml(text) {
